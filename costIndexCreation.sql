c1a7f1a77ac7f9cfe50ad0843bfb3f93d877e8b6
PerdizioB <perdizio.uk@gmail.com>
2024-05-12 21:51:57 +0100

    create procedure

--- costIndexCreation.sql
+++ costIndexCreation.sql
@@ -1,28 +1,62 @@
 --Regra para saber o custo de criação do indice
-/*
-Composite Index:
-
+/* 
+- --------------------------------------------
+				COMPOSITE  INDEX
+- --------------------------------------------
 Composite index
 with 2 attributes of type float and type Integer.
 Interger Data Type: 4 bytes
 Float  Data Type: 17 bytes
-Header: 20 bytes
- 
-Formula:
+Header: 20 bytes (SQL) *96
+
+- --------------------------------------------
+				FORMULA  INDEX
+ - --------------------------------------------
 
 [17 (array pairs) * 1 (attribute) + 4 (array pairs) * 1 (attribute)]+[21 (occupied spaces)]+2 (special space estimate)=42
 FillFactor: 70% (SQL)
-Each Page: 8192*30% Bytes (But Fillfactor= 70%)= 5735
+Each Page: 8192*30% Bytes (But Fillfactor= 70%)= 5735 --30 em espaço vazio
 5735-20 (Header)= 5715 of space to occupy by the index.
+ 
+ - --------------------------------------------
+				Simple  INDEX
+ - --------------------------------------------
 
-5715/43=132 (pages)
-6001215(Tuple) /132=45.463(paginas)
+with 1 attribute of type Integer.
+Interger Data Type: 4 bytes
+Header: 96 bytes (SQL) 96 Cada pagina começa com um cabeçalho de 96 paginas
+
+Formula:
+
+[ 4 (array pairs) * 1 (attribute)]+[4 (occupied spaces)]+2 (special space estimate)=10
+FillFactor: 70% (SQL)
+Each Page: 8192 bytes
+100%-30%=70%
+8192-30% (Fillfactor= 70%) = 2457
+2457-96 (Header)=2361 Space to occupy by the index.
+
+select count(*) from lineitem
+2361/10=136.1 (pages)
+6001215(Tuple- registro na tabela) /136.1=45.463(paginas)
 
 Parametros da proc: lineitem(tabela),coluna
 
 
+exec  [dbo].[cosIndexCreation] 'l_extendedprice', 'lineitem'
+
+--Quando for composto terá que usar cursor
 
 */
+
+ALTER PROCEDURE [dbo].[cosIndexCreation]
+
+
+@coluna varchar(100),
+@tabela varchar(100)
+
+AS
+BEGIN
+
 
 
 Create table #TB_MAX(
@@ -33,30 +67,30 @@ tamanho FLOAT
 
 
 DECLARE
-		@coluna varchar(50),
-		@cur varchar(100),
+		--@coluna varchar(50),
+		@cursor varchar(100),
 		@aux varchar(100),
 		@header int,
 		@spaceIndex int,
 		@page int,
 		@tuple int,
-		@tabela varchar(50),
+		--@tabela varchar(50),
 	    @columnVal varchar(100)
 
 
 
 set @spaceIndex=5715
 set @tuple= (select count(*) from lineitem)
-set @coluna  = 'l_extendedprice, L_PARTKEY'
+--set @coluna  = 'l_extendedprice, L_PARTKEY'
 
 DECLARE db_cursor CURSOR FOR
 
 
 SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_NAME = 'lineitem' and column_name in ('l_extendedprice','L_PARTKEY')
+WHERE TABLE_NAME = @tabela and column_name in (@coluna)
 
 OPEN db_cursor
-FETCH NEXT FROM db_cursor INTO @cur
+FETCH NEXT FROM db_cursor INTO @cursor
 
 
 WHILE @@FETCH_STATUS = 0  
@@ -64,8 +98,8 @@ BEGIN
 declare 	@soma float
 
 --Pego o tamanho do tipo
-SELECT @soma=(select  max_length FROM sys.types where name= @cur)
-FETCH NEXT FROM db_cursor INTO @cur 
+SELECT @soma=(select  max_length FROM sys.types where name= @cursor)
+FETCH NEXT FROM db_cursor INTO @cursor 
 INSERT INTO #TB_MAX values (@soma)
 
 
@@ -83,3 +117,4 @@ drop table #TB_MAX
 
 
 
+END
